32916caa1fceb789d8cf26beb4d12705845c42cc
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-08-05 16:01:56 +0200

    Added HealthCheck file to check if frontend can reach backend successful

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import HealthCheck from './pages/HealthCheck';
+import './App.css';
 
-import './App.css'
 
 function App() {
-  
+
   return (
     <div> 
       <h1>Hello Frontend World!</h1>
+      <HealthCheck />
     </div>
   )
 }
