6bd0357cfb6b1e9f9ab813380d3fa9fe6172573a
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-08-07 16:09:24 +0200

    created User types validations

---
+++ src/types/User.ts
@@ -0,0 +1,50 @@
+
+
+// What the backend sends back
+export type User = { 
+  id: string; 
+  firstName: string;
+  lastName: string; 
+  username: string;
+  email: string;
+  dateOfBirth: string;
+  gender: 'male' | 'female' | 'non-binary' | 'other' ;
+  profileImage: string | null ;
+  location: {
+    city: string;
+    country: string;
+    coordinates?: {
+        latitude: number;
+        longitude: number;
+    };
+};
+  bio: string | null;
+  sports: { sportId: string; skillLevel: string }[];
+  createdAt: string;
+};
+
+export type SignUpInput = {
+    firstName: string;
+    lastName: string;
+    username: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+    dateOfBirth: string;
+    gender:  'male' | 'female' | 'non-binary' | 'other' ;
+    location: {
+        city: string;
+        country: string;
+    };
+};
+
+export type LoginInput = {
+    identifier: string; // See 'Api contract' - accepts either an email or a username
+    password: string;
+};
+
+export type AuthResponse = {
+  user: User ;
+  token: string ;
+};
+
