0fdd538d64ba91c6a3e86547d3b7528a282ccbfa
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-08-07 18:01:17 +0200

    Added authContext

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,70 @@
+import { createContext, useState, useContext, type ReactNode } from "react";
+import type { SignUpInput, LoginInput, User } from "../types/User";
+import { signup as apiSignup, login as apiLogin } from "../api/authApi";
+
+
+interface AuthContextValue  {
+    user: User | null;
+    loading: boolean;
+    login: (data: LoginInput) => Promise<void>;
+    signup: (data: SignUpInput) => Promise<void>;
+    logout: () => void;
+
+}
+
+const AuthContext = createContext<AuthContextValue | undefined>(undefined);
+
+export default function AuthProvider({ children } : { children: ReactNode}) {
+    const [user, setUser] = useState<User | null>(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+            try {
+            return JSON.parse(storedUser);
+        } catch {
+            localStorage.removeItem('user'); // if corrupted data, clear it
+            return null;
+        }
+    }
+    return null;
+    });
+
+    const [loading, setLoading] = useState(false); //true until we check /auth/me
+
+
+    const persistSession = (nextUser: User, token: string) => {
+        localStorage.setItem('token', token);
+        localStorage.setItem('user', JSON.stringify(nextUser));
+        setUser(nextUser);
+    };
+
+    const handleSignUp = async (data: SignUpInput) => {
+        const { user: signedInUser, token } = await apiSignup(data);
+        persistSession(signedInUser, token);
+    };
+
+    const handleLogin = async (data: LoginInput) => {
+        const { user: loggedInUser, token } = await apiLogin(data);
+        persistSession(loggedInUser, token);
+    };
+
+    const handleLogout = () => {
+        setUser(null);
+        localStorage.removeItem('token');
+
+    };
+
+    return (
+    <AuthContext.Provider value={{user, loading, signup: handleSignUp, login: handleLogin, logout: handleLogout}}>
+        {children}
+        </AuthContext.Provider>
+    );
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function useAuth() {
+   const context =  useContext(AuthContext);
+   if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+   }
+   return context;
+}
