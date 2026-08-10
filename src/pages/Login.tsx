bea51dc6113ebfd9bdbf4bdd4cdc2229391d197d
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-08-10 13:06:55 +0200

    starting on login page logic

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,21 @@
+import React, {useState} from 'react';
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import type { LoginInput } from '../types/User';
+
+
 export default function LoginPage() {
+
+const [form, setForm] = useState<LoginInput>({
+
+    identifier: "",
+    password: "",
+});
+
+const { login } = useAuth();
+const navigate = useNavigate();
+
+
+
     return <h1>Login Page!</h1>;
 }
