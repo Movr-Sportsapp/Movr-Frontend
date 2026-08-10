320621a16bad899a2a1648fa16a614a0010e1608
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-08-10 14:23:45 +0200

    Added guared routes for certain pages, applied them to App.tsx routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import HealthCheck from './pages/HealthCheck';
 import HomePage from './pages/Home';
 import LoginPage from './pages/Login';
 import SignUpPage from './pages/SignUp';
+import PostEvent from './pages/PostEvent';
 import './App.css';
+import { RequireAuth, RequireGuest } from './routes/routeGuards';
 
 
 function App() {
@@ -12,8 +14,9 @@ function App() {
     <div> 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<RequireGuest> <SignUpPage /></RequireGuest>}/>
+        <Route path="/login" element={<RequireGuest><LoginPage /></RequireGuest>} />
+        <Route path="/post" element={<RequireAuth><PostEvent /></RequireAuth>} />
         <Route path="/healthcheck" element={<HealthCheck />} />  
       </Routes>
     </div>
---
+++ src/pages/PostEvent.tsx
@@ -0,0 +1,3 @@
+export default function PostEvent() {
+    return <h1> Post an event or activity!</h1>;
+}
